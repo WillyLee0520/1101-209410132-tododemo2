546c169ba9c6935816bdc6c9c25db5c618dd7586
Willy <willy0510010@gmail.com>
2021-10-07 02:41:39 +0800

    Initial commit

---
+++ react-demo/src/App_32.js
@@ -0,0 +1,15 @@
+import Todo_32 from './components/Todo_32.js';
+import './App.css';
+
+function App_32() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo_32 text='XX' /> 
+      <Todo_32 text='XXXX' /> 
+      <Todo_32 text='XXXXXXX' /> 
+    </div>
+  );
+}
+
+export default App_32;
